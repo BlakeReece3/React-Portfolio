c27eaf5611446077b22e7b8c05e460caad65117c
Blake Reece <frankiereece3@gmail.com>
2023-05-16 21:52:20 -0700

    added logos, changed css

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <footer>
       <ul>
-        <li><a href="https://github.com/BlakeReece3"><img src={require('../photos/github-mark.png')} alt="GitHub" /></a></li>
-        <li><a href="https://linkedin.com">LinkedIn</a></li>
+        <li><a href="https://github.com/BlakeReece3"><img src={require('../photos/github-mark.png')} alt="GitHub" className='logo'/></a></li>
+        <li><a href="https://www.linkedin.com/in/blake-reece"><img src={require('../photos/LI-In-Bug.png')} alt="LinkedIn" className='logo'/></a></li>
         <li><a href="https://stackoverflow.com">Stack Overflow</a></li>
       </ul>
     </footer>
